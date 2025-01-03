9b85f9281fa815f295505d50415538c15560f9d2
srinivasu Penke <127410005+srinivasupenke@users.noreply.github.com>
2025-01-03 16:53:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://localhost:5174",
+      "http://localhost:5173",
       "https://shoppy-ecommerce-website-frontend.onrender.com",
     ],
   })
