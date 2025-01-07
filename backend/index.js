b89e13785e26f967451a7b11b980144f73ec208f
srinivasupenke <srinudps123@gmail.com>
2025-01-07 16:47:50 +0530

    E-Commerse

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Origin: ", req.headers.origin); // Log the origin of the request
+  next(); // Continue with the request processing
+});
+
 app.use(
   cors({
     origin: [
