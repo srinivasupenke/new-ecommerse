e178b9e953e5b1000dbbd1f0d529ad1a6d0a24be
srinivasupenke <srinudps123@gmail.com>
2025-01-09 15:43:31 +0530

    Admin

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import "./Navbar.css";
 import nav_logo from "../../assets/nav-logo.svg";
 import navProfile from "../../assets/nav-profile.svg";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={nav_logo} alt="" className="nav-logo" />
+      <Link to="/">
+        <img src={nav_logo} alt="" className="nav-logo" />
+      </Link>
+
       <img src={navProfile} alt="" className="nav-profile" />
     </div>
   );
